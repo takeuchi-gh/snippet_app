b73de8fcfb8cea02a64e53c94e06482a04ac00c3
EC2 Default User <ec2-user@ip-172-31-38-48.ap-northeast-1.compute.internal>
2020-01-06 06:39:27 +0000

    modify_snippets_controller

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -31,7 +31,7 @@ class SnippetsController < ApplicationController
     snippet = Snippet.find(params[:id])
     snippet.language_id = params[:snippet][:language_id]
     if snippet.update(snippet_params)
-      redirect_to @snippet
+      redirect_to snippet_path
       flash[:success] = "更新しました"
     else
       flash[:danger] = "エラー"
